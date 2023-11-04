0988edf7ddd68893a9620cd29e5cb496cc312398
Ehsan <ctech1699@gmail.com>
2023-11-04 17:50:32 +0530

    up2

--- Backend/db.js
+++ Backend/db.js
@@ -4,7 +4,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 const connectToMongo = async()=>{
   try {
-    await mongoose.connect("mongodb+srv://ctech1699:ehsan2006@cluster0.npmlqlc.mongodb.net/test_apps?retryWrites=true&w=majority");
+    await mongoose.connect(MONGO_URI);
     console.log("Connected To MongoDb")
 
   } catch (error) {
